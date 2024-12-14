5996dd8ac7313b3e1f6c416c6a5dc3537f16bf01
npeled <npeled@>
2024-12-14 16:42:23 +0200

    made a docker-compose.yml to start 2 stock-services and a mongodb db that has 2 different collections (one for each stockservice)
    Dockerfile - removed unnecessary lines.
    
    main - collection name is set by the given collection env var (inorder to have 2 different collections, one for each service, using the same code).

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ def main():
     Initializes the StockController and runs the Flask app.
     """
     try:
-        mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
-        client = MongoClient(mongo_uri)
+        client = MongoClient("mongodb://mongodb:27017/")
         db = client["stock_portfolio"]
-        stocks_collection = db["stocks"]
+        stocks_collection = db[os.getenv("MONGO_COLLECTION")]
 
         controller = StockController(stocks_collection)
 
         # Run the Flask app
-        controller.app.run(host='0.0.0.0', port=5001)
+        port = int(os.getenv("FLASK_PORT"))
+        controller.app.run(host='0.0.0.0', port=port)
 
     except Exception as e:
         print(f"Error starting the Stock Portfolio Manager: {str(e)}")
